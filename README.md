dda798ab55177a5bea6f91980c79509194623185
vlastimilvasek <vlastimil.vasek@outlook.cz>
2024-02-14 09:13:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-html-to-pdf-example
+
+jspdf ve verzi 1.4.1 - vyšší verze nemají "fromHTML" funkci, ale plugin, který nejde přes npm
---
+++ src/app/app.component.ts
@@ -0,0 +1,42 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Template, BLANK_PDF } from '@pdfme/common';
+import { generate } from '@pdfme/generator';
+
+const template: Template = {
+  basePdf: BLANK_PDF,
+  schemas: [
+    {
+      a: {
+        type: 'text',
+        position: { x: 0, y: 0 },
+        width: 10,
+        height: 10,
+      },
+      b: {
+        type: 'text',
+        position: { x: 10, y: 10 },
+        width: 10,
+        height: 10,
+      },
+      c: {
+        type: 'text',
+        position: { x: 20, y: 20 },
+        width: 10,
+        height: 10,
+      },
+    },
+  ],
+};
+
+const inputs = [{ a: 'a1', b: 'b1', c: 'c1' }];
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+
+  public downloadPDF() {}
+}
